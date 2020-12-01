66b2497476e46d0140056775cbcac95675e27362
Jacob Arellano <jmm.arellano@gmail.com>
2020-11-30 20:04:56 -0700

    refine methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,27 +1,35 @@
 class Deck
-  attr_reader :cards
+  attr_reader :cards, :high
 
   def initialize(cards)
     @cards = cards
+    @high = []
   end
 
   def rank_of_card_at(index_number)
     @cards[index_number].rank
   end
 
-  def high_ranking_cards(card)
-    card
+  def high_ranking_cards
+    @cards.each do |card|
+      if card.rank >= 11
+        @high.insert(-1, card)
+      end
+    end
+    @high
   end
 
-  def percent_high_ranking(card)
-    card
+  def percent_high_ranking
+    self.high_ranking_cards
+    ((@high.length*1.00)/@cards.length) *100.00
+
   end
 
   def remove_card(card)
-    card
+    @cards.delete(card)
   end
 
   def add_card(card)
-    card
+    @cards.insert(-1, card)
   end
 end
