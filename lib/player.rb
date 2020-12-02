5270f0fe026f67122cce8821a081641c84a93104
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-01 20:36:25 -0700

    add methods and attributes

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,24 @@
-require './deck'
-require './card'
+require './lib/deck'
+require './lib/card'
 
 class Player
-  def initialize
+  attr_accessor :name, :deck, :has_lost
 
+
+  def initialize(name, deck=[])
+    @name = name
+    @deck = deck
+    @has_lost = false
   end
+
+  def has_lost?
+    if @deck.cards == []
+      @has_lost = true
+    end
+
+    @has_lost
+  end
+
+
 
 end
