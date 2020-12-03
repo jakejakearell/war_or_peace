d53635eb8b78e1b137b08131561aef15b61835fc
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-02 19:45:48 -0700

    add set-up

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,65 +4,47 @@ require './lib/card'
 require './lib/deck'
 
 class DeckTest < Minitest::Test
-  def test_rank
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, '5', 5)
-    card4 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3, card4]
-    deck = Deck.new(cards)
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, '5', 5)
+    @card4 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3, @card4]
+    @deck = Deck.new(@cards)
+  end
 
-    assert_equal 12, deck.rank_of_card_at(0)
+  def test_rank
+
+    assert_equal 12, @deck.rank_of_card_at(0)
   end
 
   def test_highest_rank
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, '5', 5)
-    card4 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3, card4]
-    deck = Deck.new(cards)
 
-    assert_equal [card1, card4], deck.high_ranking_cards
-
+    assert_equal [@card1, @card4], @deck.high_ranking_cards
 
   end
 
   def test_percent
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, '5', 5)
-    card4 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3, card4]
-    deck = Deck.new(cards)
 
-    assert_equal 50.0, deck.percent_high_ranking
+    assert_equal 50.0, @deck.percent_high_ranking
+
   end
-
+#
   def test_remove
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, '5', 5)
-    card4 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3, card4]
-    deck = Deck.new(cards)
 
-    deck.remove_card(card4)
+    @deck.remove_card(@card4)
 
-    assert_equal deck.cards, [card1, card2, card3]
+    assert_equal @deck.cards, [@card1, @card2, @card3]
+
   end
 
   def test_add
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, '5', 5)
-    card4 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
 
-    deck.add_card(card4)
+    card5 = Card.new(:heart, '6', 6)
 
-    assert_equal deck.cards, [card1, card2, card3, card4]
+    @deck.add_card(card5)
+
+    assert_equal @deck.cards, [@card1, @card2, @card3, @card4, card5]
   end
 
 end
