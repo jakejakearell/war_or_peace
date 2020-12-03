7ceca48ffe29e8051a449fe22e757cbff10a2fb8
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-02 19:45:24 -0700

    add methods

---
+++ lib/turn.rb
@@ -0,0 +1,21 @@
+class Turn
+  attr_reader :player1, :player2, :spoils_of_wars
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_wars = []
+  end
+
+  def type
+
+    if @player1.deck.cards[0].rank == @player2.deck.cards[0].rank && @player1.deck.cards[2].rank  == @player2.deck.cards[2].rank
+      :mutually_assured_destruction
+    elsif @player1.deck.cards[0].rank == @player2.deck.cards[0].rank
+      :war
+    else
+      :basic
+    end
+  end
+
+end
