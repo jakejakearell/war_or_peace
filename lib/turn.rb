04cffcab36c0982dcf70b2b0e4f385897fb05c72
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-03 17:27:03 -0700

    add iteration to pile_cards

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,4 +18,58 @@ class Turn
     end
   end
 
+  def winner
+
+    if self.type == :basic
+      if @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
+        @player1
+      else
+        @player2
+      end
+
+    elsif self.type == :war
+      if @player1.deck.cards[2].rank > @player2.deck.cards[2].rank
+        @player1
+      else
+        @player2
+      end
+
+    else
+      "No Winner"
+
+    end
+  end
+
+  def pile_cards
+    if self.type == :mutually_assured_destruction
+      @player1.deck.cards.shift(3)
+      @player2.deck.cards.shift(3)
+
+    elsif self.type == :war
+      3.times do
+        @spoils_of_wars << @player1.deck.cards.shift
+      end
+
+      3.times do
+        @spoils_of_wars << @player2.deck.cards.shift
+      end
+
+    else
+      spoil = @player1.deck.cards.shift
+      spoil2 = @player2.deck.cards.shift
+      @spoils_of_wars << spoil
+      @spoils_of_wars << spoil2
+
+    end
+
+
+  end
+
+  def award_spoils(winner)
+    @spoils_of_wars.each do |spoil|
+      winner.deck.cards << spoil
+    end
+
+    @spoils_of_wars = []
+  end
 end
