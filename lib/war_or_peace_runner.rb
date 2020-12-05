a942bfbc267b11ea1b1c035aaa352c06ae2d1c1c
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-04 18:04:27 -0700

    bring runner to working state

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -32,13 +32,18 @@ def deck_compiler
       cards << card4
     end
 
-    deck = Deck.new(cards)
+    deck = Deck.new(cards.shuffle)
+
 
 end
 
 def start
-
-  deck = deck_compiler
+  card = Card.new(:spade, "1", 1)
+  card2 = Card.new(:club, "1", 1)
+  card3 = Card.new(:heart, "1", 1)
+  card4 = Card.new(:heart, "4", 4)
+  cards = [card, card2, card3, card4]
+  deck = Deck.new(cards)
 
   deck1 = deck_compiler
 
@@ -50,29 +55,54 @@ def start
 
   turn = Turn.new(player1, player2)
 
-  until answer == 'GO'
-    puts "Welcome to War! (or Peace) This game will be played with 52 cards.
-  The players today are #{player1.name} and #{player2.name}.
-  Type 'GO' to start the game!
-  ------------------------------------------------------------------"
-
-    answer = gets.chomp
-  end
+  # until answer == 'GO'
+  #   puts "Welcome to War! (or Peace) This game will be played with 52 cards.
+  # The players today are #{player1.name} and #{player2.name}.
+  # Type 'GO' to start the game!
+  # ------------------------------------------------------------------"
+  #
+  #   answer = gets.chomp
+  # end
 
   until player1.has_lost? || player2.has_lost?
 
     turn.type
-    turn.pile_cards
+    puts "#{turn.type}"
+
+    player1_hand = player1.deck.cards[0..2]
+    player2_hand = player2.deck.cards[0..2]
+
+    puts "#{player1.name} hand preview"
+    player1_hand.each {|card| puts card.value}
+
+    puts "#{player2.name} hand preview"
+    player2_hand.each {|card| puts card.value}
+
+
+
     winner = turn.winner
+
+    if winner.class == String
+      puts "NO winner"
+    else
+      puts "#{winner.name} won this round. They will get"
+    end
+    # turn.spoils_of_wars.each {|war| puts war.value}
+    turn.pile_cards
+
+    turn.spoils_of_wars.each {|card| puts card.rank}
+
     turn.award_spoils(winner)
 
-    puts player1.deck.cards.length
+    player1.has_lost?
+    player2.has_lost?
+    # puts player1.deck
 
 
 
 
   end
-
+  puts "wweeeee"
 end
 
 start
