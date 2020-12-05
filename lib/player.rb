b22d0ac97aff18e9f3d13f97aaf8b60606b3ba57
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-04 18:10:31 -0700

    modify instance variables

--- lib/player.rb
+++ lib/player.rb
@@ -4,13 +4,14 @@ class Player
   attr_accessor :name, :deck, :has_lost
 
 
-  def initialize(name, deck=[])
+  def initialize(name, deck)
     @name = name
     @deck = deck
     @has_lost = false
   end
 
   def has_lost?
+    # require 'pry'; binding pry
     if @deck.cards == []
       @has_lost = true
     end
