db50bb1234f7c6aa1088d5e168a29071726c63be
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-06 17:00:21 -0700

    change format of file

--- lib/player.rb
+++ lib/player.rb
@@ -1,17 +1,14 @@
-
-
 class Player
   attr_accessor :name, :deck, :has_lost
-
 
   def initialize(name, deck)
     @name = name
     @deck = deck
     @has_lost = false
+    
   end
 
   def has_lost?
-
     if @deck.cards == []
       @has_lost = true
     end
@@ -19,7 +16,4 @@ class Player
     @has_lost
 
   end
-
-
-
 end
