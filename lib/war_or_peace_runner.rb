7f37146d75570cdd75173bcad00644fc69aafb4e
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-06 17:36:07 -0700

    fix string interpolation

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -23,7 +23,7 @@ def start
   answer = ''
   until answer == 'GO'
     puts "Welcome to War! (or Peace) This game will be played with 52 cards.
-  The players today are #{player1.name} and #{player2.name}.
+  The players today are #{player1.name.capitalize} and #{player2.name.capitalize}.
   Type 'GO' to start the game!
   ------------------------------------------------------------------"
 
